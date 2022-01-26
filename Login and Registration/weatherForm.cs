61a85c84be180c6cfee75cc6655ded2b8c48d208
LouiseWats <81812239+LouiseWats@users.noreply.github.com>
2022-01-26 14:47:18 +0100

    Rounded edges

--- Login and Registration/weatherForm.cs
+++ Login and Registration/weatherForm.cs
@@ -3,14 +3,30 @@ using System.Drawing;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using System.Net;
+using System.Runtime.InteropServices;
+
 
 namespace Login_and_Registration
 {
     public partial class weatherForm : Form
     {
+        
+        // Creating round corners on the form
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+        (
+        int nLeftRect,
+        int nTopRect,
+        int RightRect,
+        int nBottomRect,
+        int nWidthEllipse,
+        int nHeightEllipse
+        );
         public weatherForm()
         {
             InitializeComponent();
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
         }
 
         string apiKey = ""; //your api key from OpenWeather
